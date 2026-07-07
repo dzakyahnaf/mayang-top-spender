871356f28d770c453c1a1668a676118325fe27b8
Ferdi Anjay <ferdianjay5@gmail.com>
2026-07-07 13:32:16 +0700

    chore: improve login error message to Indonesian

---
+++ lang/en/auth.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'failed' => 'Email, nomor HP, atau password yang kamu masukkan salah. Silakan coba lagi.',
+    'password' => 'Password yang kamu masukkan tidak sesuai.',
+    'throttle' => 'Terlalu banyak percobaan login. Silakan coba lagi dalam :seconds detik.',
+
+];
